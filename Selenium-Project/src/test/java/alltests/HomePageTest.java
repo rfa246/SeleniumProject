68c30f9a6a40a1d8e50959a4ff8c34a60c018a69
rfa246 <reemafayaz00@gmail.com>
2022-08-18 22:30:17 -0400

    adding another line

--- Selenium-Project/src/test/java/alltests/HomePageTest.java
+++ Selenium-Project/src/test/java/alltests/HomePageTest.java
@@ -21,6 +21,7 @@ public class HomePageTest {
     public void test1(){
         System.out.println("this is a test");
         System.out.println("Testing git");
+        System.out.println("Making another");
 
     }
 }
